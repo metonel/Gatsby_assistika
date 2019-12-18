26e0e22a58365b13a295efaf75df85d6f45fe5c5
Toni Nemtisor <nemtisor.antonio@gmail.com>
2019-12-18 18:42:50 +0200

    animated header links

--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -7,168 +7,111 @@ import { Link } from "gatsby"
 import withStyles from "@material-ui/core/styles/withStyles"
 import List from "@material-ui/core/List"
 import ListItem from "@material-ui/core/ListItem"
-import Tooltip from "@material-ui/core/Tooltip"
-
-// @material-ui/icons
-import { Apps, CloudDownload } from "@material-ui/icons"
-
-// React icons
-import { FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa"
 
 // core components
 import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx"
 import Button from "components/CustomButtons/Button.jsx"
+
+import { Spring } from "react-spring/renderprops"
 
 import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.jsx"
 
 function HeaderLinks({ ...props }) {
   const { classes } = props
   return (
-    <List className={classes.list}>
-      <ListItem className={classes.listItem}>
-        <CustomDropdown
-          noLiPadding
-          buttonText="Services"
-          buttonProps={{
-            className: classes.navLink,
-            color: "transparent",
-          }}
-          // buttonIcon={Apps}
-          dropdownList={[
-            <Link to="/technical-writing" className={classes.dropdownLink}>
-              Technical writing
-            </Link>,
-            <Link to="/editing-review" className={classes.dropdownLink}>
-              Editing and review
-            </Link>,
-            <Link to="/adopt" className={classes.dropdownLink}>
-              Adopt a technical writer from us
-            </Link>,
-            <Link to="/training" className={classes.dropdownLink}>
-              Training and consulting
-            </Link>,
-          ]}
-        />
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <CustomDropdown
-          noLiPadding
-          buttonText="Expertise"
-          buttonProps={{
-            className: classes.navLink,
-            color: "transparent",
-          }}
-          // buttonIcon={Apps}
-          dropdownList={[
-            <Link
-              to="/documentation-deliverables"
-              className={classes.dropdownLink}
-            >
-              Documentation deliverables
-            </Link>,
-            <Link to="/industries" className={classes.dropdownLink}>
-              Industries we serve
-            </Link>,
-          ]}
-        />
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <Link to="/the-agency" className={classes.navLink}>
-          The Agency
-        </Link>
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <Link to="/careers" className={classes.navLink}>
-          Careers
-        </Link>
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <Button
-          style={{
-            borderRadius: "24px",
-            textTransform: "none",
-          }}
-          // href="https://www.creative-tim.com/product/material-kit-react"
-          color="hireUs"
-          target="_blank"
-          // className={classes.navLink}
-          // color="info"
-        >
-          {/* <CloudDownload className={classes.icons} /> Download */}
-          <Link style={{ fontWeight: "450", fontSize: "15px" }} to="/hire-us">
-            Hire us
-          </Link>
-          <span className="loading-dots">
-            <span className="dot one">.</span>
-            <span className="dot two">.</span>
-            <span className="dot three">.</span>
-          </span>
-        </Button>
-      </ListItem>
-      {/* <ListItem className={classes.listItem}>
-        <Tooltip
-          id="instagram-twitter"
-          title="Follow us on twitter"
-          placement={
-            typeof window !== "undefined" && window.innerWidth > 959
-              ? "top"
-              : "left"
-          }
-          classes={{ tooltip: classes.tooltip }}
-        >
-          <Button
-            href="https://twitter.com/CreativeTim"
-            target="_blank"
-            color="transparent"
-            className={classes.navLink}
-          >
-            <FaTwitter />
-          </Button>
-        </Tooltip>
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <Tooltip
-          id="instagram-facebook"
-          title="Follow us on facebook"
-          placement={
-            typeof window !== "undefined" && window.innerWidth > 959
-              ? "top"
-              : "left"
-          }
-          classes={{ tooltip: classes.tooltip }}
-        >
-          <Button
-            color="transparent"
-            href="https://www.facebook.com/CreativeTim"
-            target="_blank"
-            className={classes.navLink}
-          >
-            <FaFacebook />
-          </Button>
-        </Tooltip>
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <Tooltip
-          id="instagram-tooltip"
-          title="Follow us on instagram"
-          placement={
-            typeof window !== "undefined" && window.innerWidth > 959
-              ? "top"
-              : "left"
-          }
-          classes={{ tooltip: classes.tooltip }}
-        >
-          <Button
-            color="transparent"
-            href="https://www.instagram.com/CreativeTimOfficial"
-            target="_blank"
-            className={classes.navLink}
-          >
-            <FaInstagram />
-          </Button>
-        </Tooltip>
-      </ListItem> */}
-    </List>
+    <Spring
+      from={{ opacity: 0 }}
+      to={{ opacity: 1 }}
+      delay={300}
+      config={{ duration: 500 }}
+    >
+      {props => (
+        <div style={props}>
+          <List className={classes.list}>
+            <ListItem className={classes.listItem}>
+              <CustomDropdown
+                noLiPadding
+                buttonText="Services"
+                buttonProps={{
+                  className: classes.navLink,
+                  color: "transparent",
+                }}
+                dropdownList={[
+                  <Link
+                    to="/technical-writing"
+                    className={classes.dropdownLink}
+                  >
+                    Technical writing
+                  </Link>,
+                  <Link to="/editing-review" className={classes.dropdownLink}>
+                    Editing and review
+                  </Link>,
+                  <Link to="/adopt" className={classes.dropdownLink}>
+                    Adopt a technical writer from us
+                  </Link>,
+                  <Link to="/training" className={classes.dropdownLink}>
+                    Training and consulting
+                  </Link>,
+                ]}
+              />
+            </ListItem>
+            <ListItem className={classes.listItem}>
+              <CustomDropdown
+                noLiPadding
+                buttonText="Expertise"
+                buttonProps={{
+                  className: classes.navLink,
+                  color: "transparent",
+                }}
+                dropdownList={[
+                  <Link
+                    to="/documentation-deliverables"
+                    className={classes.dropdownLink}
+                  >
+                    Documentation deliverables
+                  </Link>,
+                  <Link to="/industries" className={classes.dropdownLink}>
+                    Industries we serve
+                  </Link>,
+                ]}
+              />
+            </ListItem>
+            <ListItem className={classes.listItem}>
+              <Link to="/the-agency" className={classes.navLink}>
+                The Agency
+              </Link>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+              <Link to="/careers" className={classes.navLink}>
+                Careers
+              </Link>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+              <Button
+                style={{
+                  borderRadius: "24px",
+                  textTransform: "none",
+                }}
+                color="hireUs"
+                target="_blank"
+              >
+                <Link
+                  style={{ fontWeight: "450", fontSize: "15px" }}
+                  to="/hire-us"
+                >
+                  Hire us
+                </Link>
+                <span className="loading-dots">
+                  <span className="dot one">.</span>
+                  <span className="dot two">.</span>
+                  <span className="dot three">.</span>
+                </span>
+              </Button>
+            </ListItem>
+          </List>
+        </div>
+      )}
+    </Spring>
   )
 }
 
